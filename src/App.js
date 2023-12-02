255b33181346553915b561cc14251575247369ed
tripaider <tripaider.com@gmail.com>
2023-12-02 14:11:19 +0530

    Adding page counter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <nav className="navbar navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">tripaider</a>
+        </div>
+      </nav>
+      <section className="container text-center">
+        <article className="pt-5 row">
+          <div className="col-md-3"></div>
+          <div className="col-md-6">
+            <h1>Coming Soon</h1>
+            <p>
+              An awesome new community for travellers is coming very soon. Enter your email to request an early
+              invitation!
+            </p>
+            <p className="fst-italic">We are nearly complete...</p>
+            <form className="row g-3 mb-3">
+              <div className="input-group">
+                <input type="email" className="form-control" id="email" placeholder="Enter Your Email" />
+                <button type="submit" className="btn btn-primary">Notify Me</button>
+              </div>
+            </form>
+            <p>We promise to never spam you.</p>
+          </div>
+        </article>
+
+        <div className="elfsight-app-218d07be-39cd-4196-823b-face916f1839" data-elfsight-app-lazy></div>
+      </section>
+      <footer className="container text-center mt-5">
+        <ul>
+          <li>
+            <a target="_blank" href="https://www.facebook.com/tripaider">facebook</a>
+          </li>
+        </ul>
+      </footer>
+    </main>
   );
 }
 
